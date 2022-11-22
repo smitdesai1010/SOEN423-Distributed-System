cb9689d1f9ce0dbc51ab32399ad443eabae6b10b
Adrien Tremblay <adrien@adrientremblay.com>
2022-11-21 20:43:20 -0500

    Replica manager spawns a replica

--- Replica Manager/ReplicaManager/src/ReplicaManager.java
+++ Replica Manager/ReplicaManager/src/ReplicaManager.java
@@ -1,2 +1,18 @@
+import java.io.File;
+import java.io.IOException;
+
 public class ReplicaManager {
+    public static void main(String[] args) {
+        ProcessBuilder pb = new ProcessBuilder("java", "-jar", "replica3.jar");
+        File f = new File("ReplicaJars");
+        System.out.println(f.exists());
+        pb.directory(f);
+
+        Process replica3;
+        try {
+            replica3 = pb.start();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
