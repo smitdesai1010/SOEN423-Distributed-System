ad7a5d287a2b82e4340471ac9153c95899ba8b5d
Smit Desai <57582187+smitdesai1010@users.noreply.github.com>
2022-11-22 19:04:20 -0500

    commit

--- FrontEnd_Sequencer/src/main/java/FrontEnd/ServerImplementation.java
+++ FrontEnd_Sequencer/src/main/java/FrontEnd/ServerImplementation.java
@@ -10,19 +10,18 @@ import javax.jws.soap.SOAPBinding;
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
+import java.util.ArrayList;
+import java.util.List;
 
 @WebService(endpointInterface = "FrontEnd.ServerInterface")
 @SOAPBinding(style = SOAPBinding.Style.RPC)
 public class ServerImplementation implements ServerInterface {
 
+    // Note: SOAP spins up a new thread automatically to handle mulitple concurrent requests
+
     @Override
     public String executeRequest(JSONObject obj) {
-
-        //create port number dynamically
-        //Do the election thing
-            //If defect, inform RM
-            //Timeout?
-        //send back one response to client
+        //TODO: Create port number dynamically
 
         Sequencer seq = new Sequencer();
         boolean multicastResult = seq.multicast(obj);
@@ -31,22 +30,59 @@ public class ServerImplementation implements ServerInterface {
             return "Internal Server error";
         }
 
-        JSONObject[] responseData = listenForResponse(1000);
+        int PORT = 1000;
+        JSONObject[] responseData = listenForResponse(PORT);
+        checkForByzantineFailure(responseData);
 
-
-        return "";
+        return returnTheConsensusResponse(responseData);
     }
 
-    public JSONObject[] listenForResponse(int PORT) {
+    private void checkForByzantineFailure(JSONObject[] responseData) {
+        int totalSuccess = 0;
+        int totalEntries = responseData.length;
 
-        int ctr = 0;
-        JSONObject[] responseData = new JSONObject[3];
+        // Find the majority answer
+        for (JSONObject obj : responseData) {
+            totalSuccess += Boolean.parseBoolean(obj.get("Success").toString()) ? 1 : -1;
+        }
+
+        // If the value of totalSuccess is not equal to totalEntries, means one replica has sent different
+        if (Math.abs(totalSuccess) != totalEntries) {
+            for (JSONObject obj : responseData) {
+                int objSucessValue = Boolean.parseBoolean(obj.get("Success").toString()) ? 1 : -1;
+
+                if (objSucessValue * totalSuccess < 0) {
+                    //TODO: Inform the defective replica
+                }
+            }
+        }
+    }
+
+    private String returnTheConsensusResponse(JSONObject[] responseData) {
+        int success = 0;
+
+        for (JSONObject obj : responseData) {
+            // If success is true, add 1. If success is false, add -1
+            success += Boolean.parseBoolean(obj.get("Success").toString()) ? 1 : -1;
+
+            //TODO: What if Total entries is 2 (one crashed cause of timeout)
+            if (success == 2 || success == -2) {
+                return obj.get("Data").toString();
+            }
+        }
+
+        return "Internal Server Error";
+    }
+
+    private JSONObject[] listenForResponse(int PORT) {
+
+        List<JSONObject> responseData = new ArrayList<>();
         JSONParser parser = new JSONParser();
         System.out.println("Starting UDP Server to wait for responses");
 
         try (DatagramSocket aSocket = new DatagramSocket(PORT)) {
 
-            while (ctr < 3) {
+            while (responseData.size() < 3) {
                 byte[] requestByteArray = new byte[10000];
                 DatagramPacket request = new DatagramPacket(requestByteArray, requestByteArray.length);
                 aSocket.receive(request);
@@ -54,22 +90,27 @@ public class ServerImplementation implements ServerInterface {
                 String requestString = new String(request.getData()).trim();
                 JSONObject requestObject = (JSONObject) parser.parse(requestString);
 
-                //store client ip and port number
-
-                responseData[ctr] = requestObject;
-                ++ctr;
-                //timeout?
-                //do I need to send back response?
+                responseData.add(requestObject);
+                //TODO: Store client ip and port number in the JSON
+                //TODO: Check for Timeout?
+                //TODO: Do I need to send back response to RM that I received the data?
             }
         }
         catch (IOException | ParseException e) {
             System.out.println("Error on UDP Server " + e.getMessage());
         }
 
-        return responseData;
+        return responseData.toArray(new JSONObject[0]);
     }
 
+    //TODO: Implementation
+    public void sendUDPMessage(String IP, int PORT, JSONObject data) {
+
+    }
 }
+
+// use dynamic port number
+// what message to send on failure or crash(timeout) detection
 
 // use multicastpublisher
 // total ordering
