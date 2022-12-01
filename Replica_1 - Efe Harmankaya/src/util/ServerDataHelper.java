a789bd779c14846c3285d72d8bfe514638339d28
Efe Harmankaya <82821119+efeharmankaya@users.noreply.github.com>
2022-12-01 13:59:30 -0500

    change initial server data to shared data

--- Replica_1 - Efe Harmankaya/src/util/ServerDataHelper.java
+++ Replica_1 - Efe Harmankaya/src/util/ServerDataHelper.java
@@ -4,6 +4,62 @@ import java.util.HashMap;
 
 import util.IDTRS.EventType;
 
+/*
+    Montreal {
+        ArtGallery : {
+            { eventId: "MTLM010123", guests: ["MTLP5555", "TORP5555"], capacity: 5 },
+            { eventId: "MTLA010123", guests: ["MTLP1111", "MTLP2222", "MTLP3333", "MTLP4444", "MTLP5555"], capacity: 0 },
+            { eventId: "MTLE010123", guests: [], capacity: 10 }
+        },
+        Concerts: {
+            { eventId: "MTLM020123", guests: ["MTLP2222"], capacity: 80 },
+            { eventId: "MTLA020123", guests: ["MTLP4444", "MTLP5555", "VANP5555", "VANP2222"], capacity: 1 },
+            { eventId: "MTLE020123", guests: ["VANP5555", "VANP4444"], capacity: 5 }
+        },
+        Theatre: {
+            { eventId: "MTLM030123", guests: ["MTLP2222", "MTLP5555"], capacity: 80 },
+            { eventId: "MTLA030123", guests: ["MTLP4444", "MTLP5555", "VANP5555", "VANP2222"], capacity: 1 },
+            { eventId: "MTLE030123", guests: ["VANP5555", "VANP2222"], capacity: 5 }
+        }
+    }
+
+    Toronto {
+        ArtGallery : {
+            { eventId: "TORM010123", guests: ["MTLP5555", "TORP5555"], capacity: 5 },
+            { eventId: "TORA010123", guests: ["TORP1111", "TORP2222", "MTLP3333", "MTLP5555"], capacity: 0 },
+            { eventId: "TORE010123", guests: ["VANP2222"], capacity: 10 }
+        },
+        Concerts: {
+            { eventId: "TORM020123", guests: ["MTLP2222"], capacity: 99 },
+            { eventId: "TORA020123", guests: ["TORP5555", "MTLP5555", "VANP5555", "VANP2222"], capacity: 1 },
+            { eventId: "TORE020123", guests: ["VANP5555", "VANP4444"], capacity: 5 }
+        },
+        Theatre: {
+            { eventId: "TORM030123", guests: ["MTLP2222", "MTLP5555"], capacity: 4 },
+            { eventId: "TORA030123", guests: ["MTLP4444", "MTLP5555", "VANP5555", "VANP2222"], capacity: 1 },
+            { eventId: "TORE030123", guests: ["VANP5555", "VANP2222"], capacity: 2 }
+        }
+    }
+
+    Vancouver {
+        ArtGallery : {
+            { eventId: "VANM010123", guests: ["MTLP5555", "VANP5555"], capacity: 5 },
+            { eventId: "VANA010123", guests: ["VANP1111", "VANP2222", "MTLP3333", "MTLP5555"], capacity: 0 },
+            { eventId: "VANE010123", guests: ["VANP2222"], capacity: 10 }
+        },
+        Concerts: {
+            { eventId: "VANM020123", guests: ["MTLP2222"], capacity: 99 },
+            { eventId: "VANA020123", guests: ["VANP5555", "MTLP5555", "VANP5555", "VANP2222"], capacity: 1 },
+            { eventId: "VANE020123", guests: ["VANP5555", "VANP4444"], capacity: 5 }
+        },
+        Theatre: {
+            { eventId: "VANM030123", guests: ["MTLP2222", "MTLP5555"], capacity: 4 },
+            { eventId: "VANA030123", guests: ["MTLP4444", "MTLP5555", "VANP5555", "VANP2222"], capacity: 1 },
+            { eventId: "VANE030123", guests: ["VANP5555", "VANP2222"], capacity: 2 }
+        }
+    }
+ */
+
 public class ServerDataHelper {
     public static HashMap<EventType, HashMap<String, EventData>> getStartupData(String city) {
         switch (city) {
@@ -23,24 +79,24 @@ public class ServerDataHelper {
         {
             put(EventType.ArtGallery, new HashMap<String, EventData>() {
                 {
-                    put("MTLM010122", new EventData(5, new String[] { "MTLP5555", "TORP5555" }));
-                    put("MTLA010122", new EventData(0,
+                    put("MTLM010123", new EventData(5, new String[] { "MTLP5555", "TORP5555" }));
+                    put("MTLA010123", new EventData(0,
                             new String[] { "MTLP1111", "MTLP2222", "MTLP3333", "MTLP4444", "MTLP5555" }));
-                    put("MTLE010122", new EventData(15));
+                    put("MTLE010122", new EventData(10));
                 }
             });
             put(EventType.Concerts, new HashMap<String, EventData>() {
                 {
-                    put("MTLM020122", new EventData(5, new String[] { "MTLP5555", "TORP5555" }));
-                    put("MTLA020122", new EventData(3, new String[] { "MTLP2222" }));
-                    put("MTLE020122", new EventData(15, new String[] { "TORP5555" }));
+                    put("MTLM020123", new EventData(80, new String[] { "MTLP2222" }));
+                    put("MTLA020123", new EventData(1, new String[] { "MTLP4444", "MTLP5555", "VANP5555", "VANP2222" }));
+                    put("MTLE020123", new EventData(5, new String[] { "VANP5555", "VANP4444" }));
                 }
             });
             put(EventType.Theatre, new HashMap<String, EventData>() {
                 {
-                    put("MTLM030122", new EventData(5, new String[] { "MTLP5555" }));
-                    put("MTLA030122", new EventData(3, new String[] { "VANP5555", "VANP2222" }));
-                    put("MTLE030122", new EventData(15, new String[] { "MTLP2222" }));
+                    put("MTLM030123", new EventData(80, new String[] { "MTLP2222", "MTLP5555" }));
+                    put("MTLA030123", new EventData(1, new String[] { "MTLP4444", "MTLP5555", "VANP5555", "VANP2222" }));
+                    put("MTLE030123", new EventData(5, new String[] { "VANP5555", "VANP2222" }));
                 }
             });
         }
@@ -50,24 +106,24 @@ public class ServerDataHelper {
         {
             put(EventType.ArtGallery, new HashMap<String, EventData>() {
                 {
-                    put("TORM010122", new EventData(5, new String[] { "MTLP5555", "TORP5555" }));
-                    put("TORA010122", new EventData(0,
-                            new String[] { "MTLP1111", "MTLP2222", "MTLP3333", "MTLP4444", "MTLP5555" }));
-                    put("TORE010122", new EventData(15));
+                    put("TORM010123", new EventData(5, new String[] { "MTLP5555", "TORP5555" }));
+                    put("TORA010123", new EventData(0,
+                            new String[] { "TORP1111", "TORP2222", "MTLP3333", "MTLP5555" }));
+                    put("TORE010123", new EventData(10, new String[] { "VANP2222" }));
                 }
             });
             put(EventType.Concerts, new HashMap<String, EventData>() {
                 {
-                    put("TORM020122", new EventData(5, new String[] { "MTLP5555", "TORP5555" }));
-                    put("TORA020122", new EventData(3, new String[] { "MTLP2222" }));
-                    put("TORE020122", new EventData(15, new String[] { "TORP5555" }));
+                    put("TORM020123", new EventData(99, new String[] { "MTLP2222" }));
+                    put("TORA020123", new EventData(1, new String[] { "TORP5555", "MTLP5555", "VANP5555", "VANP2222" }));
+                    put("TORE020123", new EventData(5, new String[] { "VANP5555", "VANP4444" }));
                 }
             });
             put(EventType.Theatre, new HashMap<String, EventData>() {
                 {
-                    put("TORM030122", new EventData(5, new String[] { "MTLP5555" }));
-                    put("TORA030122", new EventData(3, new String[] { "VANP5555", "VANP2222" }));
-                    put("TORE030122", new EventData(15, new String[] { "MTLP2222" }));
+                    put("TORM030123", new EventData(4, new String[] { "MTLP2222", "MTLP5555" }));
+                    put("TORA030123", new EventData(1, new String[] { "MTLP4444", "MTLP5555", "VANP5555", "VANP2222" }));
+                    put("TORE030123", new EventData(2, new String[] { "VANP5555", "VANP2222" }));
                 }
             });
         }
@@ -77,24 +133,24 @@ public class ServerDataHelper {
         {
             put(EventType.ArtGallery, new HashMap<String, EventData>() {
                 {
-                    put("VANM010122", new EventData(5, new String[] { "MTLP5555", "TORP5555" }));
-                    put("VANA010122", new EventData(0,
-                            new String[] { "MTLP1111", "MTLP2222", "MTLP3333", "MTLP4444", "MTLP5555" }));
-                    put("VANE010122", new EventData(15));
+                    put("VANM010123", new EventData(5, new String[] { "MTLP5555", "VANP5555" }));
+                    put("VANA010123", new EventData(0,
+                            new String[] { "VANP1111", "VANP2222", "MTLP3333", "MTLP5555" }));
+                    put("VANE010123", new EventData(10, new String[] { "VANP2222" }));
                 }
             });
             put(EventType.Concerts, new HashMap<String, EventData>() {
                 {
-                    put("VANM020122", new EventData(5, new String[] { "MTLP5555", "TORP5555" }));
-                    put("VANA020122", new EventData(3, new String[] { "MTLP2222" }));
-                    put("VANE020122", new EventData(15, new String[] { "TORP5555" }));
+                    put("VANM020123", new EventData(99, new String[] { "MTLP2222" }));
+                    put("VANA020123", new EventData(1, new String[] { "VANP5555", "MTLP5555", "VANP5555", "VANP2222" }));
+                    put("VANE020123", new EventData(5, new String[] { "VANP5555", "VANP4444" }));
                 }
             });
             put(EventType.Theatre, new HashMap<String, EventData>() {
                 {
-                    put("VANM030122", new EventData(5, new String[] { "MTLP5555" }));
-                    put("VANA030122", new EventData(3, new String[] { "VANP5555", "VANP2222" }));
-                    put("VANE030122", new EventData(15, new String[] { "MTLP2222" }));
+                    put("VANM030123", new EventData(4, new String[] { "MTLP2222", "MTLP5555" }));
+                    put("VANA030123", new EventData(1, new String[] { "MTLP4444", "MTLP5555", "VANP5555", "VANP2222" }));
+                    put("VANE030123", new EventData(2, new String[] { "VANP5555", "VANP2222" }));
                 }
             });
         }
